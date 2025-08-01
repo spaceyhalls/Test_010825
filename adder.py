2b5f3633c10204373763a3fba47c22077e26c78e
SPH <Serena.Pacey-Halls@ons.gov.uk>
2025-08-01 10:32:44 +0100

    Update adder.py
    We introduced functionality for a third variable (c)

--- adder.py
+++ adder.py
@@ -5,8 +5,8 @@ Created on Fri Aug  1 10:24:16 2025
 @author: paceys
 """
 
-def adder(a,b):
-    answer = a+b
+def adder(a,b,c):
+    answer = a+b+c
     return answer
 
-print(adder(3,4)==7)
+print(adder(3,4,0)==7)
